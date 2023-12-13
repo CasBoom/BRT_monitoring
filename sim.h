48df2d88d90dc319eb7a27517d2bf5b56c46baa5
Casper Boom <casper.boom@rysst.nl>
2023-12-13 10:33:40 +0100

    add bufferCommand function

--- sim.h
+++ sim.h
@@ -14,11 +14,29 @@
 extern "C" {
 #endif
 
+// send a simple AT command
 bool sendCommand(char* command, int length){
     char buffer[32];
     printLn(command, length);
     read(&buffer, sizeof(buffer)/sizeof(char));
     return (buffer[0]=='O' && buffer[1]=='K'); //returns true if OK, otherwise returns false;
+}
+
+// send AT command with non-standard expected response
+bool sendCommand(char* command, int tLength, char* response, int rLength){
+    char buffer[32];
+    printLn(command, tLength);
+    read(&buffer, sizeof(buffer)/sizeof(char));
+    for(int i=0; i<rLength; i++){
+        if(response+i != buffer+i)return false;
+    }
+    return true;
+}
+
+//sends response to buffer for more involved response flows
+void bufferCommand(char* command, int tLength, char* buffer, int bLength){
+    printLn(command, tLength);
+    read(&buffer, sizeof(buffer)/sizeof(char));
 }
 
 struct SIM{
@@ -33,7 +51,9 @@ uint8_t SIMCardExists(){
     // check if simcard exists
     // return 1 if it does
     // return 0 if not
-    return 1;
+    char* command="AT+CDIN=?";
+    char* response="READY";
+    return sendCommand(command, sizeof(command)/sizeof(char), response, sizeof(response)/sizeof(char));
 }
 
 uint8_t makeConnection(){
@@ -58,7 +78,7 @@ uint8_t setupSIM(struct SIM* sim){
     //attempt to setup sim
     sim->card=0;
     sim->connection=0;
-    if(!SIMCardExists()) return 1; // return 1 if the 
+    if(!SIMCardExists()) return 1; // return 1 if the SimCard doesn't exist
     sim->card=1;
     // turn on gnss with AT commands, once OK ask update
     if(updateCoordinates(sim)) return 3;
